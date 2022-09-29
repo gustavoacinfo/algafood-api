88d362400dadbd24eadfd1b483bb7a2874575f2f
Gustavo Alves <gustavo.acinfo@gmail.com>
2022-09-29 17:09:38 -0300

    Criando entidade Permissao e seus inserts

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,3 +8,7 @@ insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida I
 insert into forma_pagamento (descricao) values ('Boleto');
 insert into forma_pagamento (descricao) values ('Cartao de credito');
 insert into forma_pagamento (descricao) values ('Cartao de debito');
+
+insert into permissao (nome, descricao) values ('Consultar Restaurante', 'Permite ao Usuário acesso a lista de Restaurantes');
+insert into permissao (nome, descricao) values ('Consultar Cozinha', 'Permite ao Usuário acesso a lista de Cozinhas');
+insert into permissao (nome, descricao) values ('Consultar Pedido', 'Permite ao Usuário acesso a lista de Pedidos');
