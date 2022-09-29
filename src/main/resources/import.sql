f961e9041b02e1c9f95ec86b1e6033ec8f0a0436
Gustavo Alves <gustavo.acinfo@gmail.com>
2022-09-29 16:52:30 -0300

    Criando entidade FormaPagamento e seus inserts

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,7 @@ insert into cozinha (id, nome) values (2, 'Indiana');
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 2);
+
+insert into forma_pagamento (descricao) values ('Boleto');
+insert into forma_pagamento (descricao) values ('Cartao de credito');
+insert into forma_pagamento (descricao) values ('Cartao de debito');
