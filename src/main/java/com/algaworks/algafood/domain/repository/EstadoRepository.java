54e10979cb7cfde5e8bf737d8527deedc94166d9
Gustavo Alves <gustavo.acinfo@gmail.com>
2022-10-03 17:49:45 -0300

    Implementando o Collection Resource de Estados

--- src/main/java/com/algaworks/algafood/domain/repository/EstadoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/EstadoRepository.java
@@ -6,7 +6,7 @@ import com.algaworks.algafood.domain.model.Estado;
 
 public interface EstadoRepository {
 	
-	List<Estado> todas();
+	List<Estado> listar();
 	Estado porId(Long id);
 	Estado adicionar(Estado estado);
 	void remover(Estado estado);
