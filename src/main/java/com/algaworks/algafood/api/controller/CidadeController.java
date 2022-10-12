c5e900a3d6174b8f55fbe25d3dadd210939b6899
Gustavo Alves <gustavo.acinfo@gmail.com>
2022-10-12 11:15:56 -0300

    Implementando a atualizacao de recursos de cidades

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -2,17 +2,21 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CidadeRepository;
 import com.algaworks.algafood.domain.service.CadastroCidadeService;
 
@@ -43,5 +47,26 @@ public class CidadeController {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 		
+	}
+	
+	@PutMapping("/{cidadeId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long cidadeId, 
+			@RequestBody Cidade cidade){
+		Cidade cidadeAtual = cidadeRepository.buscar(cidadeId);
+		
+		if(cidadeAtual != null) {
+			
+			try {
+				BeanUtils.copyProperties(cidade, cidadeAtual, "id");
+				cidadeAtual = cadastroCidade.salvar(cidadeAtual);
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(cidadeAtual);
+			} catch (EntidadeNaoEncontradaException e) {
+				return ResponseEntity.badRequest().body(e.getMessage());
+			}
+		
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
