2450e8c882b3b6ffc3c7e0ac249e72d60750ad1c
Gustavo Alves <gustavo.acinfo@gmail.com>
2022-10-14 22:12:40 -0300

    Refatorando repositorio de cidades para utilizar Spring Data JPA

--- src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -1,7 +1,5 @@
 package com.algaworks.algafood.domain.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -25,21 +23,19 @@ public class CadastroCidadeService {
 	
 	public Cidade salvar(Cidade cidade) {
 		Long estadoId = cidade.getEstado().getId();
-		Optional<Estado> estado = estadoRepository.findById(estadoId);
 		
-		if(estado == null) {
-			throw new EntidadeNaoEncontradaException(
-				String.format("Não existe cadastro de estado com código %d", estadoId));
-		}
+		Estado estado = estadoRepository.findById(estadoId)
+			.orElseThrow(() -> new EntidadeNaoEncontradaException(
+				String.format("Não existe cadastro de estado com código %d", estadoId)));
 		
-		cidade.setEstado(estado.get());
+		cidade.setEstado(estado);
 		
-		return cidadeRepository.salvar(cidade);
+		return cidadeRepository.save(cidade);
 	}
 	
 	public void excluir(Long cidadeId) {
 		try {
-			cidadeRepository.remover(cidadeId);
+			cidadeRepository.deleteById(cidadeId);
 			
 		} catch (EmptyResultDataAccessException e){
 			throw new EntidadeNaoEncontradaException(
