90abce1bfae3bceb9988f8c7ae6b676edd917548
Gustavo Alves <gustavo.acinfo@gmail.com>
2022-12-08 12:45:50 -0300

    Desafio: tratando outras exceções não capturadas

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -26,6 +26,8 @@ import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 	
+	
+	
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -49,7 +51,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 		
 	}
 	
-	
 	@Override
 	protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
@@ -65,9 +66,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), 
 				status, request);	
 	}
-
-
-
 
 	@Override
 	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
@@ -175,6 +173,27 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), 
 				status, request);
 	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> handleUncaught(
+			Exception ex, WebRequest request){
+		
+		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+		ProblemType problemType = ProblemType.ERRO_DE_SISTEMA;
+		String detail= String.format("Ocorreu um erro interno inesperado no sistema. Tente novamente e se o "
+				+ "problema persistir, entre em contato com o administrador do sistema.");
+		
+		ex.printStackTrace();
+		
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), 
+				status, request);
+	}
+	
+	
+	
 	
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
