3e065cc61b1a6bf7d43bbc8cd0a7b4c3e668be50
Gustavo Alves <gustavo.acinfo@gmail.com>
2023-01-19 22:11:16 -0300

    Refatorando projeto para usar OffsetDateTime em atributos de data/hora

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
-import java.time.LocalDateTime;
+
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -268,14 +269,14 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 			body = Problem.builder()
 				.title(status.getReasonPhrase())
 				.status(status.value())
-				.timestamp(LocalDateTime.now())
+				.timestamp(OffsetDateTime.now())
 				.userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL)
 				.build();
 		}else if (body instanceof String) {
 			body = Problem.builder()
 				.title((String) body)
 				.status(status.value())
-				.timestamp(LocalDateTime.now())
+				.timestamp(OffsetDateTime.now())
 				.userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL)
 				.build();
 		}
@@ -292,7 +293,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 				.type(problemType.getUri())
 				.title(problemType.getTitle())
 				.detail(detail)
-				.timestamp(LocalDateTime.now());
+				.timestamp(OffsetDateTime.now());
 	}
 	
 	private String joinPath(List<Reference> references) {
