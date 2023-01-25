478249269e8fd905052b54cbd22d19a7fd030ac6
Gustavo Alves <gustavo.acinfo@gmail.com>
2023-01-24 21:42:49 -0300

    Adicionando e usando o ModelMapper

---
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModalMapperConfig.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.core.modelmapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModalMapperConfig {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
+	
+
+}
--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -10,9 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
-import com.algaworks.algafood.core.validation.Groups;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
