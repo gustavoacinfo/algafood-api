08e699a934281103a9f684b72bdd7231187a9f50
Gustavo Alves <gustavo.acinfo@gmail.com>
2023-02-04 16:06:53 -0300

    Implementando os endpoints de ativação e inativação de restaurantes

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -73,19 +74,26 @@ public class RestauranteController {
 			@RequestBody @Valid RestauranteInput restauranteInput){
 		
 		try {
-//			Restaurante restaurante = restauranteInputDesassembler.toDomainObject(restauranteInput);
-
 			Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 		
 			restauranteInputDesassembler.copyToDomainObject(restauranteInput, restauranteAtual);
 			
-//			BeanUtils.copyProperties(restaurante, restauranteAtual, 
-//				"id", "formasPagamento", "endereco", "dataCadastro", "produtos");
-		
 			return restauranteModelAssembler.toModel(cadastroRestaurante.salvar(restauranteAtual));
 		} catch (CozinhaNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage());
 		}
+	}
+	
+	@PutMapping("/{restauranteId}/ativo")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void ativar(@PathVariable Long restauranteId) {
+		cadastroRestaurante.ativar(restauranteId);
+	}
+	
+	@DeleteMapping("/{restauranteId}/ativo")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void inativar(@PathVariable Long restauranteId) {
+		cadastroRestaurante.inativar(restauranteId);
 	}
 	
 	
