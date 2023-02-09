8f5cba938d2c6b79f013478f4209347aa2f9d157
Gustavo Alves <gustavo.acinfo@gmail.com>
2023-02-08 22:10:49 -0300

    Corrigindo anotacao @Repository para os repositorios de Estado e Grupo

--- src/main/java/com/algaworks/algafood/domain/repository/GrupoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/GrupoRepository.java
@@ -1,11 +1,11 @@
 package com.algaworks.algafood.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Grupo;
 
-@RestController
+@Repository
 public interface GrupoRepository extends JpaRepository<Grupo, Long>{
 
 }
