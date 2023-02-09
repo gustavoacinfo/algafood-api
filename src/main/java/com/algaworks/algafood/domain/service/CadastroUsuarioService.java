d6aa73f98da4a3009b63cf13f0a77f89094fe6ca
Gustavo Alves <gustavo.acinfo@gmail.com>
2023-02-09 20:05:57 -0300

    Criando classe de serviço para Usuário

---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
+import com.algaworks.algafood.domain.model.Usuario;
+import com.algaworks.algafood.domain.repository.UsuarioRepository;
+
+@Service
+public class CadastroUsuarioService {
+	
+//	private static final String MSG_USUARIO_EM_USO = "Usuário de código %d não pode ser removido, pois está em uso!";
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Transactional
+	public Usuario salvar(Usuario usuario) {
+		return usuarioRepository.save(usuario);
+	}
+	
+	
+	public Usuario buscarOuFalhar(Long usuarioId) {
+		return usuarioRepository.findById(usuarioId)
+				.orElseThrow(() -> new EstadoNaoEncontradoException(usuarioId));
+	}
+
+}
