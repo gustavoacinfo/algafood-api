d0b73697bbb1e878e034b5b16b9ef7059ab1a428
Gustavo Alves <gustavo.acinfo@gmail.com>
2023-02-14 20:53:43 -0300

    Criando endpoint para buscar um unico produto de um restaurante

--- src/main/java/com/algaworks/algafood/api/controller/ProdutoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/ProdutoController.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.assembler.ProdutoModelAssembler;
+import com.algaworks.algafood.api.model.CozinhaModel;
 import com.algaworks.algafood.api.model.ProdutoModel;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Produto;
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.service.CadastroProdutoService;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 
 @RestController
@@ -23,11 +27,23 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoModelAssembler produtoModelAssembler;
 	
+	@Autowired
+	private CadastroProdutoService cadastroProduto;
+	
 	@GetMapping
 	public List<ProdutoModel> listar(@PathVariable Long restauranteId){
 		Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
 		
 		return produtoModelAssembler.toCollectionModel(restaurante.getProdutos());
+	}
+	
+	@GetMapping("/{produtoId}")
+	public ProdutoModel buscar(@PathVariable Long produtoId, @PathVariable Long restauranteId) {
+		Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
+		
+		Produto produto = cadastroProduto.buscarOuFalhar(produtoId, restaurante.getId());
+
+		return produtoModelAssembler.toModel(produto);
 	}
 
 }
