9d7e4c663c147a37a9ede826d0bc8d2f5a1557de
Gustavo Alves <gustavo.acinfo@gmail.com>
2023-03-22 19:43:34 -0300

    Implementando consulta com dados agregados de vendas diárias

--- src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,10 @@ import com.algaworks.algafood.domain.service.VendaQueryService;
 @RequestMapping(path = "/estatisticas")
 public class EstatisticasController {
 	
+	@Autowired
 	private VendaQueryService vendaQueryService;
 	
-	@GetMapping
+	@GetMapping("/vendas-diarias")
 	public List<VendaDiaria> consultarVendasDiarias(VendasDiariasFilter filtro){
 		return vendaQueryService.consultarVendasDiarias(filtro);
 	}
