82f505587a821c10001830ecef82c85759247754
Gustavo Alves <gustavo.acinfo@gmail.com>
2023-03-27 21:32:18 -0300

    Mapeando entidade FotoProduto e relacionamento um-para-um

---
+++ src/main/java/com/algaworks/algafood/domain/model/FotoProduto.java
@@ -0,0 +1,38 @@
+package com.algaworks.algafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.algaworks.algafood.core.validation.Groups;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class FotoProduto {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@Column(name = "produto_id")
+	private Long id;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@MapsId
+	private Produto produto;
+	
+	private String nomeArquivo;
+	private String descricao;
+	private String contentType;
+	private Long tamanho;
+	
+}
