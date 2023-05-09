d3eee7c075c306ed10b8ac23849a2cea8271fa40
Gustavo Alves <gustavo.acinfo@gmail.com>
2023-05-08 22:36:57 -0300

    Implementando o serviço de infraestrutura de envio de e-mails com Spring

---
+++ src/main/java/com/algaworks/algafood/domain/service/EnvioEmailService.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.domain.service;
+
+import java.util.Set;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public interface EnvioEmailService {
+	
+	void enviar(Mensagem mensagem);
+	
+	@Getter
+	@Setter
+	class Mensagem {
+		
+		private Set<String> destinatarios;
+		private String assunto;
+		private String corpo;
+		
+	}
+
+}
---
+++ src/main/java/com/algaworks/algafood/infrastructure/service/email/EmailException.java
@@ -0,0 +1,18 @@
+package com.algaworks.algafood.infrastructure.service.email;
+
+public class EmailException extends RuntimeException {
+
+	private static final long serialVersionUID = 7592435867476757995L;
+
+	public EmailException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public EmailException(String message) {
+		super(message);
+	}
+
+	
+	
+	
+}
