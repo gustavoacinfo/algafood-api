a1b54ecaf82d15e87159ea8257c345e71efe3134
Gustavo Alves <gustavo.acinfo@gmail.com>
2023-05-10 21:53:55 -0300

    Usando o serviço de envio de e-mails na confirmação de pedidos

--- src/main/java/com/algaworks/algafood/domain/service/EnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/domain/service/EnvioEmailService.java
@@ -2,19 +2,26 @@ package com.algaworks.algafood.domain.service;
 
 import java.util.Set;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NonNull;
+import lombok.Singular;
 
 public interface EnvioEmailService {
 	
 	void enviar(Mensagem mensagem);
 	
 	@Getter
-	@Setter
+	@Builder
 	class Mensagem {
 		
+		@Singular
 		private Set<String> destinatarios;
+		
+		@NonNull
 		private String assunto;
+		
+		@NonNull
 		private String corpo;
 		
 	}
